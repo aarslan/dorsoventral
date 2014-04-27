7ae9672f928a6dc8bb3ad21d49fe5e5895364826
aarslan <aarslan@g6.(none)>
2014-04-27 14:22:55 -0400

    first commit

--- ccv_motion_frame_core.sh
+++ ccv_motion_frame_core.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-#SBATCH --time=04:00:00
+#SBATCH --time=00:05:00
 ##SBATCH -n 1
 ##SBATCH -N 8-16 
 #SBATCH --qos=bibs-tserre-condo
 ##SBATCH --exclusive
 #SBATCH --exclude=smp012,smp013,smp014,smp015
 
-#SBATCH -J 10day_pos
-#SBATCH -o zcommon_scripts/10day_pos%j.out
-#SBATCH -e zcommon_scripts/10day_pos%j_e.out
+#SBATCH -J motion_xtract_trial
+#SBATCH -o out/motion_xtract_trial%j.out
+#SBATCH -e out/motion_xtract_trial%j_e.out
 
-module add matlab
-module add cuda
 
-src_code_dir='/gpfs/home/xl1/src/cns_mouse'
+src_code_dir='/users/aarslan/code/dorsoventral'
 
 
 python $src_code_dir/process_directory_motion.py --src_dir $1 --deg_l $2 --deg_r $3 --act $4 --seq $5 --target_dir $6 --this_frame 5
