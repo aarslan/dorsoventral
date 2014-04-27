e32aeef9d9cf1bd599137505516cbf57b3e4b4c0
aarslan <aarslan@g6.(none)>
2014-04-27 15:49:47 -0400

    asd

--- ccv_motion_frame_core.sh
+++ ccv_motion_frame_core.sh
@@ -16,9 +16,8 @@ module unload python
 module load enthought
 module unload cuda
 
-echo 'HERE WE GO'
+echo 'processing' $2 $3 $4 $5 $6
 src_code_dir='/users/aarslan/code/dorsoventral'
 
 
 python $src_code_dir/process_directory_motion.py --src_dir $1 --deg_r $2 --deg_l $3 --act $4 --seq $5 --target_dir $6 --this_fr 5
-
