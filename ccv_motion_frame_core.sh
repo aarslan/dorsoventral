bcabecb582bcaf7b9c88f13878737de532a38fdd
aarslan <aarslan@g6.(none)>
2014-04-28 13:10:37 -0400

    trying gnu parallel

--- ccv_motion_frame_core.sh
+++ ccv_motion_frame_core.sh
@@ -8,8 +8,7 @@
 #SBATCH --exclude=smp012,smp013,smp014,smp015
 
 #SBATCH -J motion_xtract_trial
-#SBATCH -o /users/aarslan/out/motion_xtract_trial%j.out
-#SBATCH -e /users/aarslan/out/motion_xtract_trial%j_e.out
+#SBATCH -o /users/aarslan/out/motion_xtract_%j.out
 
 
 module unload python
@@ -18,6 +17,17 @@ module unload cuda
 
 echo 'processing' $8 $2 $3 $4 $5 
 src_code_dir='/users/aarslan/code/dorsoventral'
+joblist='/users/aarslan/joblists/'$8_$2_$3_$4_$5'.jlist'
 
 
-python $src_code_dir/process_directory_motion.py --src_dir $1 --deg_r $2 --deg_l $3 --act $4 --seq $5 --target_dir $6 --this_fr $7 --body_type $8
+#python $src_code_dir/process_directory_motion.py --src_dir $1 --deg_r $2 --deg_l $3 --act $4 --seq $5 --target_dir $6 --this_fr $7 --body_type $8
+
+
+rm $joblist -f
+
+for fr in {1..45}
+do
+echo $src_code_dir/process_directory_motion.py --src_dir $1 --deg_r $2 --deg_l $3 --act $4 --seq $5 --target_dir $6 --this_fr $fr --body_type $7 >> $joblist
+done
+
+parallel  -a dnm $joblist
--- ccv_motion_frame_wrapper.sh
+++ ccv_motion_frame_wrapper.sh
@@ -10,7 +10,7 @@ data_out='/gpfs/data/tserre/Users/aarslan/motion_morphing_dataset_stereo/feature
 
 declare -a bodies=("human" "a" "y")
 
-for deg in {32..360..30} #{2..360..30}
+for deg in {2..360..30} #{2..360..30}
 
 do 
 	for bod in human a y
@@ -19,11 +19,8 @@ do
 		do
 			for seq in $action/*
 			do
-				for fr in {1..45}
-				do
-				sbatch ./ccv_motion_frame_core.sh $data_in $(($deg+6)) $deg `basename $action` `basename $seq` $data_out $fr $bod; 
+				sbatch ./ccv_motion_frame_core.sh $data_in $(($deg+6)) $deg `basename $action` `basename $seq` $data_out $bod; 
 				sleep 0.5
-				done
 			done
 		done
 	done
